fc12a6cd428dd49de0f671c63a6ff95eb40b1ab5
MeganBC <123363704+MeganBC@users.noreply.github.com>
2023-04-19 14:55:34 +0100

    fixed jump

--- Assets/Megan/Scripts/Player.cs
+++ Assets/Megan/Scripts/Player.cs
@@ -9,6 +9,12 @@ public class Player : MonoBehaviour
 
     public float movementSpeed;
     public float jumpForce;
+    public int maxJumps = 2;
+
+    int currentJumps = 0;
+    bool isOnGround;
+    public float maxSlope = 0.5f;
+
     private float horizontal;
     private float vertical;
 
@@ -23,19 +29,57 @@ public class Player : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        horizontalForce.x = horizontal * movementSpeed * Time.deltaTime * 100; 
+        horizontalForce.x = horizontal * movementSpeed * Time.deltaTime * 100;
         body.AddForce(horizontalForce);
 
-        if (Input.GetKeyDown(KeyCode.Space)) { 
-        verticalForce.y = jumpForce;
-        body.AddForce(verticalForce, ForceMode2D.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && currentJumps <= maxJumps)
+        {
+            verticalForce.y = jumpForce;
+            body.AddForce(verticalForce, ForceMode2D.Impulse);
+            currentJumps++;
+            isOnGround = false;
         }
+        
+    }
+    bool CanJump()
+    {
+        return isOnGround || currentJumps < maxJumps;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        CheckIfOnGround(collision);
 
+        if (collision.gameObject.CompareTag("Death"))
+        {
+            body.velocity = Vector2.zero;
+        }
+    }
+    
+    void CheckIfOnGround(Collision2D collision)
+    {
+        if (!isOnGround)
+        {
+            if (collision.contacts.Length > 0)
+            {
+                ContactPoint2D contact = collision.contacts[0];
+
+                float dot = Vector2.Dot(contact.normal, Vector2.up);
+
+                isOnGround = dot >= maxSlope;
+                if (isOnGround)
+                {
+                    currentJumps = 0;
+                }
+            }
+        }
     }
 }
 
-        
-    
+
+
+
+
+
 
 
 
