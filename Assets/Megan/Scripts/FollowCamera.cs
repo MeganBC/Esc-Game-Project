0ea8048f36c7f833466c79c4d168cbc279e3ee9e
MeganBC <123363704+MeganBC@users.noreply.github.com>
2023-04-26 15:25:15 +0100

    level done up to boss room (needs collisions yet)

--- Assets/Megan/Scripts/FollowCamera.cs
+++ Assets/Megan/Scripts/FollowCamera.cs
@@ -10,7 +10,7 @@ public class FollowCamera : MonoBehaviour
     void Update()
     {
         targetPosition.x = ObjectToFollow.position.x;
-        targetPosition.y = ObjectToFollow.position.y;
+        targetPosition.y = ObjectToFollow.position.y + 3;
         targetPosition.z = transform.position.z;
 
         transform.position = targetPosition;
